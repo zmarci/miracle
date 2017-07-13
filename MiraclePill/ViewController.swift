ad1b0e5bf1de94bc3574e4fa6963ef06a3b537a3
zmarci <z.marton13@gmail.com>
2017-07-13 09:08:35 +0200

    practice commit

---
+++ MiraclePill/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MiraclePill
+//
+//  Created by Marton Zatrok on 2017. 07. 13..
+//  Copyright © 2017. Marton Zatrok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
